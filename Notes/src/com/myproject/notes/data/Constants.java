e0b73b5c0404a0bb9a7028f9dfa5319faa5f5b64
yohanes86 <vincentius.yohanes86@gmail.com>
2015-08-16 20:12:10 +0700

    Update Constants.java

--- Notes/src/com/myproject/notes/data/Constants.java
+++ Notes/src/com/myproject/notes/data/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	public static final String FORMAT_DATE_DB 		= "yyyy-MM-dd HH:mm:ss";
 	public static final String FORMAT_DATE_DISPLAY  = "MMM dd, yyyy HH:mm:ss a";
 	public static final String KEY 		    		= "Gundam123+";
-	public static final String DEFAULT_PASS 		= "viperviper";
+	public static final String DEFAULT_PASS 		= "emob";
 	
 	public static final int EDIT 					= 0;
 	public static final int DELETE 					= 1;
